a71e3dd1fd9c9ee56c72b6ffa273f693fab4f9d2
Zulfahmi <142126150+ZulfahmiCP@users.noreply.github.com>
2025-09-01 09:11:18 +0700

    tambah tugas faktorial, fibonacci, dan biner

--- factorial.dart
+++ factorial.dart
@@ -1,6 +1,6 @@
 int faktorial(int n) {
     if(n < 0) 
-        throw ArgumentError('Input tidak boleh bilangan negatif.');
+        throw ArgumentError('Input tidak boleh bilangan negatif');
     if(n == 0)
         return 1;
     return n * faktorial(n - 1);
